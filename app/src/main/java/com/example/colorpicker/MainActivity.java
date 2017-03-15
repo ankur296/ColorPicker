e75687eb651e4ab9f77e220a2f51a520b720753e
ankur296 <ankur296@gmail.com>
2017-03-15 11:19:08 +0530

    b4 back

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-  //b4  b44
+  //b4  b44 back
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
